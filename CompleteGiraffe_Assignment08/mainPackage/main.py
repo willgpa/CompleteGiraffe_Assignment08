a19f66c671c7019cb0249b6056a34f50d1dec4d6
willgpa <wgpadgett@gmail.com>
2024-10-30 19:12:26 -0400

    Added to the main

--- CompleteGiraffe_Assignment08/mainPackage/main.py
+++ CompleteGiraffe_Assignment08/mainPackage/main.py
@@ -1,1 +1,15 @@
+from product import Product
 
+if __name__ == "__main__":
+    product = Product("123456789012")
+    print(product)  
+
+    # Test restocking
+    product.restock_product()  
+
+    # Test setting a new UPC
+    product.set_UPC("987654321098")
+    print("Updated Product:", product)
+
+    # Test __repr__
+    print(repr(product))  
